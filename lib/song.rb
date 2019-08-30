f86ee3de05ec383dc3c188dba793c8503a96738d
yaoyu bao <yaobao5354@gmail.com>
2019-08-30 04:55:15 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Song 
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genres
+    genres = []
+    @@genres.each do |genre|
+      if !genres.include?(genre)
+        genres << genre
+      end 
+    end 
+    genres
+  end 
+  
+end 
